981f01fba7edc49ce9746acf452ec050ecb9c6c8
Onel Medina <43082323+onelmv@users.noreply.github.com>
2020-07-14 20:54:28 -0400

    adding dinamic table

--- src/components/ImageRecognition/ImageRecognition.js
+++ src/components/ImageRecognition/ImageRecognition.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './ImageRecognition.css'
 
 const ImageRecognition =({url, ingredients})=>{
-/* 
-    let block = "";
-    let i =0;
-    while(ingredients[i].value >= 0.80){
-        block += `<li>${ingredients[i]}</li>`
-    }
- */
+
+    
+ 
+
+        let block = [];
+        
+        for(let i=0; i< ingredients.length;i++){
+            /* const {name, value} = i; */
+            console.log(ingredients[i].name)
+            block.push(<tr><td>{ingredients[i].name}</td><td>{ingredients[i].value}</td></tr>)
+        }
+        console.log(block)
+        
 
 
     return (
@@ -23,32 +29,18 @@ const ImageRecognition =({url, ingredients})=>{
                         <table cellpadding="0" cellspacing="0" border="0">
                         <thead>
                             <tr>
-                                <th>Code</th>
-                                <th>Company</th>
+                                <th>Ingredients</th>
+                                <th>Probability</th>
                             </tr>
                         </thead>
                         </table>
                     </div>
                     <div class="tbl-content">
                         <table cellpadding="0" cellspacing="0" border="0">
-                        {/*     <tbody>
-                                <tr>
-                                    <td>AAC</td>
-                                    <td>AUSTRALIAN COMPANY </td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td>AAD</td>
-                                    <td>AUSENCO</td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td>AAX</td>
-                                    <td>ADELAIDE</td>
-                                    
-                                </tr>
+                            <tbody>
                                 
-                            </tbody> */}
+                                {block.map(i=>i)}
+                            </tbody>
                         </table>
                     </div>
                 </section>
