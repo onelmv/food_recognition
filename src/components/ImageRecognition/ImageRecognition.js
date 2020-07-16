7189141ac8ead54fa743af10ae39080b994dbbf3
Onel Medina <43082323+onelmv@users.noreply.github.com>
2020-07-15 23:28:50 -0400

    table of ingredient fixed

--- src/components/ImageRecognition/ImageRecognition.js
+++ src/components/ImageRecognition/ImageRecognition.js
@@ -4,48 +4,59 @@ import './ImageRecognition.css'
 const ImageRecognition =({url, ingredients})=>{
 
     
- 
-
+    const dataTable=()=>{
+        
         let block = [];
         
         for(let i=0; i< ingredients.length;i++){
-            /* const {name, value} = i; */
+           
             console.log(ingredients[i].name)
             block.push(<tr><td>{ingredients[i].name}</td><td>{ingredients[i].value}</td></tr>)
         }
         console.log(block)
-        
+        return block;
+    }
 
+  
+    const generateTable=()=>{
+        if(url){
+
+            return (
+                <div className='border'>
+                    
+                    <section>
+      
+                       <div class="tbl-header">
+                            <table cellpadding="0" cellspacing="0" border="0">
+                            <thead>
+                                <tr>
+                                    <th>Ingredients</th>
+                                    <th>Probability</th>
+                                </tr>
+                            </thead>
+                            </table>
+                        </div>
+                        <div class="tbl-content">
+                            <table cellpadding="0" cellspacing="0" border="0">
+                                <tbody >
+                                    
+                                    {dataTable().map(i=>i)}
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+                  
+                </div>
+            )
+        }
+    }
+        
+     
 
     return (
         <div className='center w-70 ma3' >
             <img src={url} alt=""/>
-            <div className='border'>
-                
-                <section>
-  
-                    <h1>Fixed Table header</h1>
-                    <div class="tbl-header">
-                        <table cellpadding="0" cellspacing="0" border="0">
-                        <thead>
-                            <tr>
-                                <th>Ingredients</th>
-                                <th>Probability</th>
-                            </tr>
-                        </thead>
-                        </table>
-                    </div>
-                    <div class="tbl-content">
-                        <table cellpadding="0" cellspacing="0" border="0">
-                            <tbody>
-                                
-                                {block.map(i=>i)}
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
-              
-            </div>
+            {generateTable()}
         </div>
     )
 }
